c88c8439c134d069330dbf4db9f43d0cea08eaeb
Rempai <jamie.vanwingerden@gmail.com>
2024-05-22 16:28:05 +0200

    I guess

---
+++ my-svelte-app/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+    let data = '';
+
+    const fetchData = async () => {
+        const response = await fetch('/api/data');
+        const result = await response.json();
+        data = result.message;
+    };
+
+    fetchData();
+</script>
+
+<main>
+    <h1>{data}</h1>
+</main>
